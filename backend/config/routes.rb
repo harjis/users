07b9bd5250397bdc936f9c85346dd6b61f0c7dfb
Joonas Harjukallio <harjukallio@gmail.com>
2020-08-25 13:19:54 +0300

    Add graphiql service & user(id) graphql resolver

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
+  post "/api/graphql", to: "graphql#execute"
   scope :api do
     resources :users
     post 'users/validate'
