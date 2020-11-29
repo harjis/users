dadc004a1cd0c451d1bc2d49f8ae5d4dabbf3c0b
Joonas Harjukallio <harjukallio@gmail.com>
2020-11-29 16:40:19 +0200

    Hmph

--- authenticator/src/index.ts
+++ authenticator/src/index.ts
@@ -1,14 +1,17 @@
+import proxy from "express-http-proxy";
 import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.set("port", 5000);
 
+// app.get("/", proxy('localhost:3000/api/users'));
 app.get("/", (req, res) => {
   res.send("Welcome to Node Babel");
 });
 
-app.listen(5000, () => {
+app.listen(5000, "172.18.0.4", () => {
   console.log(`app is listening to port 5000`);
 });
