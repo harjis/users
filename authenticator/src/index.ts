bbdb241dd86a87b9f3bd00bbdb0e1d62fdfc1bd2
Joonas Harjukallio <harjukallio@gmail.com>
2020-11-30 11:37:47 +0200

    Start with authenticator

--- authenticator/src/index.ts
+++ authenticator/src/index.ts
@@ -1,11 +1,36 @@
-import proxy from "express-http-proxy";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import { createProxyMiddleware } from "http-proxy-middleware";
+declare module "@okta/jwt-verifier";
+import OktaJwtVerifier from "@okta/jwt-verifier";
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: "0oa6jw73iiZlcYvor357",
+  issuer: "https://dev-556018.okta.com/oauth2/default",
+});
 
 const app = express();
 
-app.use("/", proxy("backend:3000"));
+app.use(
+  "/",
+  createProxyMiddleware({ target: "http://backend:3000", changeOrigin: true })
+);
+app.use(cors());
 app.use(bodyParser.json());
+
+// app.use(async (req, res, next) => {
+//   try {
+//     if (!req.headers.authorization)
+//       throw new Error("Authorization header is required");
+//
+//     const accessToken = req.headers.authorization.trim().split(" ")[1];
+//     await oktaJwtVerifier.verifyAccessToken(accessToken, "api://default");
+//     next();
+//   } catch (error) {
+//     next(error.message);
+//   }
+// });
 
 app.listen(5000, () => {
   console.log(`app is listening to port 5000`);
