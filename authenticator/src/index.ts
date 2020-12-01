3683540fc83ef5b6fbab9c600929256f05ca6aad
Joonas Harjukallio <harjukallio@gmail.com>
2020-12-01 22:15:37 +0200

    Remove not needed & start with auth

--- authenticator/src/index.ts
+++ authenticator/src/index.ts
@@ -3,33 +3,34 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import OktaJwtVerifier from "@okta/jwt-verifier";
-//
-// const oktaJwtVerifier = new OktaJwtVerifier({
-//   clientId: "0oa6jw73iiZlcYvor357",
-//   issuer: "https://dev-556018.okta.com/oauth2/default",
-// });
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: "0oa6jw73iiZlcYvor357",
+  issuer: "https://dev-556018.okta.com/oauth2/default",
+});
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(async (req, res, next) => {
+  try {
+    if (!req.headers.authorization)
+      throw new Error("Authorization header is required");
+
+    const accessToken = req.headers.authorization.trim().split(" ")[1];
+    await oktaJwtVerifier.verifyAccessToken(accessToken, "api://default");
+    next();
+  } catch (error) {
+    next(error.message);
+  }
+});
 
 app.use(
   "/",
   createProxyMiddleware({ target: "http://backend:3000", changeOrigin: true })
 );
-app.use(cors());
-app.use(bodyParser.json());
-
-// app.use(async (req, res, next) => {
-//   try {
-//     if (!req.headers.authorization)
-//       throw new Error("Authorization header is required");
-//
-//     const accessToken = req.headers.authorization.trim().split(" ")[1];
-//     await oktaJwtVerifier.verifyAccessToken(accessToken, "api://default");
-//     next();
-//   } catch (error) {
-//     next(error.message);
-//   }
-// });
 
 app.listen(5000, () => {
   console.log(`app is listening to port 5000`);
