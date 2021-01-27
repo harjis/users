b3d99637b233e564cc6b8c59a8463d4eea1635ed
Joonas Harjukallio <harjukallio@gmail.com>
2021-01-27 16:36:59 +0200

    Deploy to production

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+docker build -t d0rka/users-backend:latest -t d0rka/users-backend:$SHA -f ./backend/Dockerfile ./backend
+docker build -t d0rka/users-frontend:latest -t d0rka/users-frontend:$SHA -f ./frontend/Dockerfile ./frontend
+
+docker push d0rka/users-backend:latest
+docker push d0rka/users-frontend:latest
+
+docker push d0rka/users-backend:$SHA
+docker push d0rka/users-frontend:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/backend-deployment backend=d0rka/users-backend:$SHA
+kubectl set image deployments/frontend-deployment frontend=d0rka/users-frontend:$SHA
+
+kubectl apply -f k8s-db-jobs/db-create.yaml
+kubectl apply -f k8s-db-jobs/db-migrate.yaml
+
+kubectl wait --for=condition=complete --timeout=600s job/db-migrate
+
+kubectl delete job db-create
+kubectl delete job db-migrate
