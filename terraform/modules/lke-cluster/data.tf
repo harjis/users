253f3e87ce53cf654a472326d1939ef31c33bb5d
Joonas Harjukallio <harjukallio@gmail.com>
2021-10-23 10:47:59 +0300

    k8s folder

--- terraform/modules/lke-cluster/data.tf
+++ terraform/modules/lke-cluster/data.tf
@@ -1,5 +1,5 @@
 data "kubectl_path_documents" "backend-service-manifests" {
-  pattern = "${path.module}/backend-service.yaml"
+  pattern = "${path.module}/k8s/backend-service.yaml"
   vars = {
     docker_image      = "d0rka/users-backend:${var.gitsha}"
     postgres_username = var.postgres_username
@@ -8,22 +8,22 @@ data "kubectl_path_documents" "backend-service-manifests" {
 }
 
 data "kubectl_path_documents" "frontend-service-manifests" {
-  pattern = "${path.module}/frontend-service.yaml"
+  pattern = "${path.module}/k8s/frontend-service.yaml"
   vars = {
     docker_image = "d0rka/users-frontend:${var.gitsha}"
   }
 }
 
 data "kubectl_path_documents" "postgres-pvc-manifests" {
-  pattern = "${path.module}/db-pvc.yaml"
+  pattern = "${path.module}/k8s/db-pvc.yaml"
 }
 
 data "kubectl_path_documents" "postgres-service-manifests" {
-  pattern = "${path.module}/postgres-service.yaml"
+  pattern = "${path.module}/k8s/postgres-service.yaml"
 }
 
 data "kubectl_path_documents" "create-db-manifests" {
-  pattern = "${path.module}/create-users-prod.yaml"
+  pattern = "${path.module}/k8s/create-users-prod.yaml"
 
   vars = {
     gitsha            = var.gitsha
@@ -33,7 +33,7 @@ data "kubectl_path_documents" "create-db-manifests" {
 }
 
 data "kubectl_path_documents" "migrate-db-manifests" {
-  pattern = "${path.module}/migrate-users-prod.yaml"
+  pattern = "${path.module}/k8s/migrate-users-prod.yaml"
 
   vars = {
     gitsha            = var.gitsha
